99213d64908fcb55fef23510debb7811e5661193
olive <olive@OLIVERS-BRBAR>
2024-02-27 15:44:19 +0100

    Add project files.

---
+++ Shortcuts/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.Design;
+
+namespace Shortcuts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //cw + tap + tap laver en Console.WriteLine();
+
+            //if + tap + tap laver en if statement
+
+            //else + tap + tap laver en else statement
+
+            //gå ind i Shortcut class
+
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Shortcuts/Shortcut.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shortcuts
+{
+    public class Shortcut
+    {
+        //Attributer
+        private string _name;
+        private string _description;
+
+        //propfull + tap + tap laver en get set
+       
+       //ctor + tap + tap laver en constructor for klasse den er i
+
+    }
+}
